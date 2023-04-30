69316b4da24b457198eb1142a89445476e759328
Robiin007 <129576912+Robiin007@users.noreply.github.com>
2023-04-30 18:17:14 +0000

    multiply is added

---
+++ index.js
@@ -0,0 +1,5 @@
+const math = require("robin-math");
+
+console.log(math.sum(3, 4));
+console.log(math.subtract(3, 4));
+console.log(math.multiply(3, 4));
--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,10 @@
 
-const sum = require('./sum');
+const math = require("robin-math");
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(math.sum(1, 2)).toBe(3);
 });
 
 test('adds 0+ 100 to equal 100', () => {
-    expect(sum(0, 100)).toBe(100);
+    expect(math.sum(0, 100)).toBe(100);
 });
